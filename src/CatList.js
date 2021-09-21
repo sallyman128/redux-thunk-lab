adb58469fc0b90dc3253f6cb90cf182274771d49
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-21 17:47:36 -0400

    commit tree 0ec84199614d8f8037d5f265fc17645bfaa2ebc6

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,14 @@
 // write your CatList component here
+import React from 'react'
+
+export const CatList = (props) => {
+  return (
+    <div>
+      {props.catPics.map( cat => {
+        return (
+          <img src={cat.url} key={cat.id} alt="Cat" />
+        )
+      })}
+    </div>
+  )
+}
